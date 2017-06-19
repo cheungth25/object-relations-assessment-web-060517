c4f7cb02651ff9846f6b0425a73918f35068b0d6
cheungth25 <cheungth25@gmail.com>
2017-06-19 10:15:26 -0400

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,30 @@
 class Customer
   attr_accessor :first_name, :last_name
-
+  @@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.find_by_name(name)
+    self.all.select { |customer| customer.first_name == name }
+  end
+
+  def self.all_names
+    self.all.map { |customer| "#{customer.first_name} #{customer.last_name}" }
+  end
+
+  def add_review(restaurant, content)
+    new_review = Review.new(self, restaurant, content)
+    new_review
   end
 end
--- solution.rb
+++ solution.rb
@@ -1,1 +1,80 @@
 # Please copy/paste all three classes into this file to submit your solution!
+class Customer
+  attr_accessor :first_name, :last_name
+  @@all = []
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def self.find_by_name(name)
+    self.all.select { |customer| customer.first_name == name }
+  end
+
+  def self.all_names
+    self.all.map { |customer| "#{customer.first_name} #{customer.last_name}" }
+  end
+
+  def add_review(restaurant, content)
+    new_review = Review.new(self, restaurant, content)
+    new_review
+  end
+end
+
+
+#==============================================================================
+
+
+
+class Restaurant
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.detect { |restaurant| restaurant.name == name }
+  end
+
+  def reviews
+    Review.all.select { |review| review.restaurant == self}
+  end
+
+  def customers
+    self.reviews.map { |review| review.customer}
+  end
+end
+
+
+#==============================================================================
+
+class Review
+  attr_accessor :customer, :restaurant, :content
+  @@all = []
+
+  def initialize(customer, restaurant, content)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
